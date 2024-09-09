151a610a4e3e637f8c8ab02e022952f372ddc0d9
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-09-09 09:50:25 +0530

    Refactor static file handling and add CSS stylesheets
    This commit refactors the code to handle static files and adds CSS stylesheets for the createUser and index views. The app now uses the Express static middleware to serve static files from the \public\ directory. Additionally, the createUser view now includes a link to the \createUser.css\ stylesheet, and the index view includes a link to the \index.css\ stylesheet. These stylesheets define the visual appearance of the respective views.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const usersRouter = require("./routes/usersRouter");
+const path = require("path")
 
-app.set("view engine", "ejs");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", usersRouter);
